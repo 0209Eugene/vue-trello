285ab24c93bd4a6b141d91fee04dc950a092a0d3
Yuto Miura <yuto.ym0209@gmail.com>
2022-01-12 22:30:52 +0900

    add basic component

--- src/components/Board.vue
+++ src/components/Board.vue
@@ -2,16 +2,37 @@
   <div>
     <header>vue Trello</header>
     <main>
-      <p class="info-line">All: 0 tasks</p>
-      <list-add />
+      <p class="info-line">All: {{ totalCardCount }} tasks</p>
+      <div class="list-index">
+        <list v-for="(item, index) in lists"
+          :key="item.id"
+          :title="item.title"
+          :cards="item.cards"
+          :listIndex="index"
+          />
+        <list-add />
+      </div>
     </main>
   </div>
 </template>
 
 <script>
 import ListAdd from './ListAdd.vue'
+import List from './List.vue'
+import { mapState } from 'vuex'
+
 export default {
-  components: { ListAdd },
-  ListAdd
+  components: { 
+    ListAdd,
+    List
+  },
+  computed: {
+    ...mapState([
+      'lists'
+    ]),
+    totalCardCount() {
+      return this.$store.getters.totalCardCount
+    },
+  },
 }
 </script>
---
+++ src/components/List.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="list">
+    <div class="listheader">
+      <p class="list-title"> {{ title }}</p>
+      <div class="list-counter">total: {{ totalCardInList }}</div>
+      <div class="deletelist" @click="removeList">X</div>
+    </div>
+    <card v-for="(item, index) in cards"
+      :body="item.body"
+      :key="item.id"
+      :cardIndex="index"
+      :listIndex="listIndex"
+    />
+    <card-add :listIndex="listIndex" />
+  </div>
+</template>
+<script>
+import CardAdd from './CardAdd.vue'
+import Card from './Card.vue'
+
+export default {
+  components: {
+    CardAdd,
+    Card,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true
+    },
+    cards: {
+      type: Array,
+      required: true
+    },
+    listIndex: {
+      type: Number,
+      required: true
+    },
+  },
+  computed: {
+    totalCardInList() {
+      return this.cards.length
+    }
+  },
+  methods: {
+    removeList: function() {
+      if (confirm('本当にこのリストを削除しますか？')){
+        this.$store.dispatch('removelist', { listIndex: this.listIndex })
+      }
+    },
+
+    },
+  }
+</script>
